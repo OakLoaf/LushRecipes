8249bcd2cdb979a0d9d2c7839201f12e9d9a4359
OakLoaf <oak@beaconstudios.org>
2025-05-23 18:12:00 +0100

    Refactored

--- src/main/java/org/lushplugins/lushrecipes/LushRecipes.java
+++ src/main/java/org/lushplugins/lushrecipes/LushRecipes.java
@@ -1,9 +1,9 @@
-package org.lushplugins.pluginname;
+package org.lushplugins.lushrecipes;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class PluginName extends JavaPlugin {
-    private static PluginName plugin;
+public final class LushRecipes extends JavaPlugin {
+    private static LushRecipes plugin;
 
     @Override
     public void onLoad() {
@@ -20,7 +20,7 @@ public final class PluginName extends JavaPlugin {
         // Disable implementation
     }
 
-    public static PluginName getInstance() {
+    public static LushRecipes getInstance() {
         return plugin;
     }
 }
