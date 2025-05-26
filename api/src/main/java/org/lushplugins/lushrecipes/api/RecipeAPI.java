88b8ea4ecc91ba243530991e4e8b5df8109fc5ef
OakLoaf <oak@beaconstudios.org>
2025-05-26 14:35:21 +0100

    Adjusted recipe discovery implementation

--- api/src/main/java/org/lushplugins/lushrecipes/api/RecipeAPI.java
+++ api/src/main/java/org/lushplugins/lushrecipes/api/RecipeAPI.java
@@ -1,8 +1,11 @@
 package org.lushplugins.lushrecipes.api;
 
+import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.Recipe;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.Unmodifiable;
 import org.lushplugins.lushrecipes.api.listener.CraftListener;
@@ -17,12 +20,15 @@ public class RecipeAPI {
     private final Map<NamespacedKey, CraftingRecipe> recipes;
     private final boolean showInRecipeBook;
 
-    private RecipeAPI(JavaPlugin plugin, Map<NamespacedKey, CraftingRecipe> recipes, boolean showInRecipeBook) {
+    private RecipeAPI(JavaPlugin plugin, Map<NamespacedKey, CraftingRecipe> recipes, boolean showInRecipeBook, boolean discoverRecipesOnJoin) {
         this.recipes = recipes;
         this.showInRecipeBook = showInRecipeBook;
 
         registerListener(new CraftListener(this), plugin);
-        registerListener(new PlayerListener(this), plugin);
+
+        if (discoverRecipesOnJoin) {
+            registerListener(new PlayerListener(this), plugin);
+        }
     }
 
     private void registerListener(Listener listener, JavaPlugin plugin) {
@@ -30,12 +36,51 @@ public class RecipeAPI {
         plugin.getServer().getPluginManager().registerEvents(listener, plugin);
     }
 
-    public void registerRecipe(NamespacedKey key, CraftingRecipe recipe) {
+    public void registerRecipe(CraftingRecipe recipe) {
+        NamespacedKey key = recipe.getKey();
         this.recipes.put(key, recipe);
+
+        Recipe bukkitRecipe = Bukkit.getRecipe(key);
+        if (bukkitRecipe != null) {
+            Bukkit.removeRecipe(key);
+        }
+
+        Bukkit.addRecipe(recipe.createBukkitRecipe(), true);
+
+        if (this.showInRecipeBook) {
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                if (recipe.canCraft(player)) {
+                    player.discoverRecipe(key);
+                }
+            }
+        }
     }
 
     public void registerRecipes(Map<NamespacedKey, CraftingRecipe> recipes) {
         this.recipes.putAll(recipes);
+
+        for (CraftingRecipe recipe : recipes.values()) {
+            NamespacedKey key = recipe.getKey();
+            Recipe bukkitRecipe = Bukkit.getRecipe(key);
+            if (bukkitRecipe != null) {
+                Bukkit.removeRecipe(key);
+            }
+
+            Bukkit.addRecipe(recipe.createBukkitRecipe(), true);
+        }
+
+        if (this.showInRecipeBook) {
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                List<NamespacedKey> keys = recipes.values().stream()
+                    .filter(recipe -> recipe.canCraft(player))
+                    .map(CraftingRecipe::getKey)
+                    .toList();
+
+                if (!keys.isEmpty()) {
+                    player.discoverRecipes(keys);
+                }
+            }
+        }
     }
 
     public void unregisterRecipe(NamespacedKey key) {
@@ -67,6 +112,25 @@ public class RecipeAPI {
         return this.showInRecipeBook;
     }
 
+    public void evaluateRecipeDiscovery(Player player) {
+        if (this.showInRecipeBook) {
+            Set<NamespacedKey> unlockedRecipes = new HashSet<>();
+            Set<NamespacedKey> lockedRecipes = new HashSet<>();
+            for (CraftingRecipe recipe : this.getRecipes()) {
+                if (recipe.canCraft(player)) {
+                    unlockedRecipes.add(recipe.getKey());
+                } else {
+                    lockedRecipes.add(recipe.getKey());
+                }
+            }
+
+            player.discoverRecipes(unlockedRecipes);
+            player.undiscoverRecipes(lockedRecipes);
+        } else {
+            player.undiscoverRecipes(this.getRecipeKeys());
+        }
+    }
+
     public void unregisterListeners() {
         this.listeners.forEach(HandlerList::unregisterAll);
         this.listeners.clear();
@@ -80,6 +144,7 @@ public class RecipeAPI {
         private final JavaPlugin plugin;
         private final Map<NamespacedKey, CraftingRecipe> recipes = new HashMap<>();
         private boolean showInRecipeBook = false;
+        private boolean discoverRecipesOnJoin = true;
 
         private Builder(JavaPlugin plugin) {
             this.plugin = plugin;
@@ -100,13 +165,18 @@ public class RecipeAPI {
             return this;
         }
 
-        public Builder shouldShowInRecipeBook(boolean showInRecipeBook) {
+        public Builder showInRecipeBook(boolean showInRecipeBook) {
             this.showInRecipeBook = showInRecipeBook;
             return this;
         }
 
+        public Builder discoverRecipesOnJoin(boolean discoverRecipesOnJoin) {
+            this.discoverRecipesOnJoin = discoverRecipesOnJoin;
+            return this;
+        }
+
         public RecipeAPI build() {
-            return new RecipeAPI(this.plugin, this.recipes, this.showInRecipeBook);
+            return new RecipeAPI(this.plugin, this.recipes, this.showInRecipeBook, this.discoverRecipesOnJoin);
         }
     }
 }
--- api/src/main/java/org/lushplugins/lushrecipes/api/listener/PlayerListener.java
+++ api/src/main/java/org/lushplugins/lushrecipes/api/listener/PlayerListener.java
@@ -1,15 +1,10 @@
 package org.lushplugins.lushrecipes.api.listener;
 
-import org.bukkit.NamespacedKey;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.lushplugins.lushrecipes.api.RecipeAPI;
-import org.lushplugins.lushrecipes.api.recipe.CraftingRecipe;
-
-import java.util.HashSet;
-import java.util.Set;
 
 public class PlayerListener implements Listener {
     private final RecipeAPI instance;
@@ -18,24 +13,8 @@ public class PlayerListener implements Listener {
         this.instance = instance;
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.MONITOR)
     public void onPlayerJoin(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
-        if (this.instance.shouldShowInRecipeBook()) {
-            Set<NamespacedKey> unlockedRecipes = new HashSet<>();
-            Set<NamespacedKey> lockedRecipes = new HashSet<>();
-            for (CraftingRecipe recipe : this.instance.getRecipes()) {
-                if (recipe.canCraft(player)) {
-                    unlockedRecipes.add(recipe.getKey());
-                } else {
-                    lockedRecipes.add(recipe.getKey());
-                }
-            }
-
-            player.discoverRecipes(unlockedRecipes);
-            player.undiscoverRecipes(lockedRecipes);
-        } else {
-            player.undiscoverRecipes(this.instance.getRecipeKeys());
-        }
+        this.instance.evaluateRecipeDiscovery(event.getPlayer());
     }
 }
