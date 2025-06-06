e510177dfb0ea02db060fafe04402faa58f51656
OakLoaf <oak@beaconstudios.org>
2025-06-06 14:19:54 +0100

    Bumped version to 1.0.0-alpha6

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ allprojects {
     apply(plugin = "com.gradleup.shadow")
 
     group = "org.lushplugins"
-    version = "1.0.0-alpha5"
+    version = "1.0.0-alpha6"
 
     repositories {
         mavenLocal()
