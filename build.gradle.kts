0465e32d996ae9328beb783361b156fc16418625
OakLoaf <oak@beaconstudios.org>
2025-07-16 09:01:14 +0100

    Bumped version to 1.0.0-alpha14

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ allprojects {
     apply(plugin = "com.gradleup.shadow")
 
     group = "org.lushplugins"
-    version = "1.0.0-alpha13"
+    version = "1.0.0-alpha14"
 
     repositories {
         mavenLocal()
